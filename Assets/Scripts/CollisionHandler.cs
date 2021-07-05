df66752f4c0f8e89ffbd8dcb31a9b3be124511ba
rko1985 <35703264+rko1985@users.noreply.github.com>
2021-07-05 10:56:12 -1000

    switch statement to handle collisions

---
+++ Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionHandler : MonoBehaviour
+{
+    void OnCollisionEnter(Collision other)
+    {
+        switch (other.gameObject.tag)
+        {
+            case "Friendly":
+                Debug.Log("This thing is friendly");
+                break;
+            case "Finish":
+                Debug.Log("Congrats, you finished!");
+                break;
+            case "Fuel":
+                Debug.Log("You picked up fuel");
+                break;            
+            default:
+                Debug.Log("Sorry you blew up!!!");
+                break;
+        }
+    }
+}
