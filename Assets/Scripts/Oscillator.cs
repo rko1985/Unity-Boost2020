f66694b50195ae5a7b9966234f104366aa536e1c
rko1985 <35703264+rko1985@users.noreply.github.com>
2021-07-23 08:56:43 -1000

    moving the block

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -7,7 +7,8 @@ public class Oscillator : MonoBehaviour
 
     Vector3 startingPosition;
     [SerializeField] Vector3 movementVector;
-    [SerializeField] [Range(0,1)] float movementFactor;
+    float movementFactor;
+    [SerializeField] float period = 2f;
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +19,12 @@ public class Oscillator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float cycles = Time.time / period; // continually growing over time
+        const float tau = Mathf.PI * 2; // const value of 6.283
+        float rawSinWave = Mathf.Sin(cycles * tau); //going from -1 to 1
+
+        movementFactor = (rawSinWave + 1f) / 2f; // recalulated to go from 0 to 1 so it's cleaner
+
         Vector3 offset = movementVector * movementFactor;
         transform.position = startingPosition + offset;
     }
