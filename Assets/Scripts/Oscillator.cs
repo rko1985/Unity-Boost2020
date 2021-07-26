8fb4a785f5f5be49cc03e11ea6ae4882de5e16e0
rko1985 <35703264+rko1985@users.noreply.github.com>
2021-07-26 07:29:56 -1000

    protecting against NaN

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -19,6 +19,8 @@ public class Oscillator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (period <= Mathf.Epsilon) { return; }
+
         float cycles = Time.time / period; // continually growing over time
         const float tau = Mathf.PI * 2; // const value of 6.283
         float rawSinWave = Mathf.Sin(cycles * tau); //going from -1 to 1
